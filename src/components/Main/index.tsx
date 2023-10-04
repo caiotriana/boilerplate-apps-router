91fe82a50146c7fc3c28d2ba7900eda4dc917088
Caio Triana <caio_triana@hotmail.com>
2023-10-04 16:32:27 -0300

    config styled components with next

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,9 @@
 import { Wrapper } from './styles'
 
 const Main = () => (
-	<Wrapper>
-		<h1>React Avançado</h1>
-	</Wrapper>
+  <Wrapper>
+    <h1>React Avançado</h1>
+  </Wrapper>
 )
 
 export default Main
